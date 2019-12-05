8d17bcfebcad32d84369368039d9cd3f40b64102
Danilo Novakovic <dakenzi97@gmail.com>
2019-12-05 20:48:47 +0100

    Wrote collision util and unit test for it
    Wrote util functions to detect weather circles are in collision or not with unit test

--- entities/Bullet.py
+++ entities/Bullet.py
@@ -2,7 +2,8 @@ from entities.MovableCircle import MovableCircle
 
 
 class Bullet(MovableCircle):
-    def __init__(self, x: float, y: float, velocity: float, angle: float, r: int, color: str, player_id: str):
+    def __init__(self, x: float = 0, y: float = 0, velocity: float = 0, angle: float = 0, r: int = 0,
+                 player_id: str = "", color: str = "",):
         super().__init__(x=x, y=y, velocity=velocity, angle=angle, r=r)
         self.color = color
         self.player_id = player_id
