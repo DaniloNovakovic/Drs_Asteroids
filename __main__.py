07ff3c208cb8cb3aafaac957c95ed50c5081aecb
Danilo Novakovic <dakenzi97@gmail.com>
2019-12-05 13:22:11 +0100

    Added dependency injection and fixed bug
    Added dependency injection and changed create_new in LevelFactory to have argument `level_number` default to 0 (first level)

--- __main__.py
+++ __main__.py
@@ -1,13 +1,25 @@
 import sys
 from core.Game import Game
 from core.Screen import Screen
+from core.LevelFactory import LevelFactory
+from core.KeyHandler import KeyHandler
+from core.MovementHandler import MovementHandler
+from core.CollisionHandler import CollisionHandler
 from PyQt5.QtWidgets import QApplication
 
 
 class AsteroidsGame:
     def __init__(self):
         self.screen = Screen(600, 400, "Asteroids")
-        self.game = Game(self.screen)
+
+        '''Dependency injection - here you can inject handlers/services into constructor'''
+        level_factory = LevelFactory()
+        key_handler = KeyHandler()
+        movement_handler = MovementHandler()
+        collision_handler = CollisionHandler()
+
+        self.game = Game(self.screen, level_factory=level_factory, key_handler=key_handler,
+                         movement_handler=movement_handler, collision_handler=collision_handler)
 
     def start(self):
         self.screen.show()
