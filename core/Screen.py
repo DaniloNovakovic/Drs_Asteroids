c99121dc6a65ff4e5408812a91d741c73faef540
Danilo Novakovic <dakenzi97@gmail.com>
2019-12-09 20:33:19 +0100

    Fixed bug where asteroid did not render
    Asteroid is now rendered on screen

--- core/Screen.py
+++ core/Screen.py
@@ -1,7 +1,9 @@
 from PyQt5 import QtCore
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QImage
-from PyQt5.QtWidgets import QLineEdit, QWidget
+from PyQt5.QtWidgets import QLineEdit, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+
 from entities.Asteroid import Asteroid
 from persistance.Storage import Storage
 
@@ -14,7 +16,7 @@ class Screen(QWidget):
         self.resize(x, y)
         self.setWindowTitle(name)
         self.le = QLineEdit(self)
-        self.le.move(130, 22)
+        self.le.move(10, 10)
 
     def keyPressEvent(self, event):
         super().keyPressEvent(event)
@@ -23,22 +25,19 @@ class Screen(QWidget):
     def render_storage(self, storage: Storage, elapsed_time: float = 0):
         self.le.setText(str(elapsed_time))
 
-        asteroids = storage.get_all_asteroids()
-        spacecrafts = storage.get_all_spacecrafts()
-
-        painter = QPainter(self)
-
-        for asteroid in asteroids:
-            self.drawAsteroid(painter, asteroid)
+        for asteroid in storage.get_all_asteroids():
+            self.drawAsteroid(asteroid)
         pass
 
-    def drawAsteroid(self, painter: QPainter, asteroid: Asteroid):
+    def drawAsteroid(self, asteroid: Asteroid):
         """TODO: Fix error where image is not displayed on screen properly"""
 
-        src_image = QImage(asteroid.img_abs_path)
-        if src_image.isNull():
-            print("Failed to load image %s" % asteroid.img_abs_path)
+        image = QImage(asteroid.img_abs_path)
+        painter = QPainter()
         painter.begin(self)
-
-        painter.drawImage(QPoint(100, 100), src_image)
+        painter.drawImage(asteroid.x, asteroid.y, image)
         painter.end()
+
+        label = QLabel(self)
+        label.setPixmap(QPixmap.fromImage(image))
+        label.show()
