41987700100569a62ebcbd6c0734e177e7b04973
vladatoskic <58035525+vladatoskic@users.noreply.github.com>
2019-12-30 16:21:25 +0100

    Implementacija EXIT dugmeta

--- Clinet/__init__.py
+++ Clinet/__init__.py
@@ -19,23 +19,32 @@ class MyWindow(QMainWindow):
         self.b1 = QtWidgets.QPushButton(self)
         self.b1.setText("START GAME")
         self.b1.setGeometry(400, 200, 250, 50)
-        self.b1.setStyleSheet("border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
+        self.b1.setStyleSheet(
+            "border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
 
         self.b2 = QtWidgets.QPushButton(self)
         self.b2.setText("SCORE")
         self.b2.setGeometry(400, 300, 250, 50)
-        self.b2.setStyleSheet("border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
+        self.b2.setStyleSheet(
+            "border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
 
         self.b3 = QtWidgets.QPushButton(self)
         self.b3.setText("EXIT")
         self.b3.setGeometry(400, 400, 250, 50)
-        self.b3.setStyleSheet("border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
+        self.b3.setStyleSheet(
+            "border:2px solid rgb(120, 20, 60); color: blue;font-size: 26px; font-family: Arial Black;");
+        self.b3.clicked.connect(self.quit)
 
     def backGround(self):
         self.BackGround = QPixmap("galaxy.jpg")
         self.BackGroundLabel = QtWidgets.QLabel(self)
         self.BackGroundLabel.setPixmap(self.BackGround)
-        self.BackGroundLabel.setGeometry(0,0,1000,600)
+        self.BackGroundLabel.setGeometry(0, 0, 1000, 600)
+
+    def quit(self):
+        app = QApplication.instance()
+        app.closeAllWindows()
+
 
 def window():
     app = QApplication(sys.argv)
