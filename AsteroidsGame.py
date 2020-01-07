8a3afa20b89503d05e82a8fa2a6e1a3843d5855e
vladatoskic <58035525+vladatoskic@users.noreply.github.com>
2020-01-07 13:25:50 +0100

    Dodato je cuvanje username-a i bodova u fajl

--- AsteroidsGame.py
+++ AsteroidsGame.py
@@ -1,3 +1,4 @@
+import string
 import sys
 from datetime import datetime
 
@@ -15,6 +16,25 @@ from core.utils.heart_factory import HeartFactory
 from core.utils.player_factory import PlayerFactory
 from core.utils.spaceship_factory import SpaceshipFactory
 from entities.PlayerInput import PlayerInput
+from persistance.Storage import Storage
+
+
+def create_text_from_storage(storage):
+    scores = []
+    for player in storage.players:
+        scores.append(f"\n{player.player_id},{player.num_points}")
+
+    text ="".join(scores)
+    print(text)
+    return text
+
+
+def save_score_to_file(storage: Storage):
+    print("saving to file..")
+    with open('test.txt', 'a') as f:
+        my_text = create_text_from_storage(storage)
+        f.write(my_text)
+    exit()
 
 
 class AsteroidsGame:
@@ -39,7 +59,8 @@ class AsteroidsGame:
         collision_handler = CollisionHandler(screen_width=screen_width, screen_height=screen_height)
 
         self.game = Game(self.screen, level_factory=level_factory, key_handler=key_handler,
-                         collision_handler=collision_handler, movement_handler=movement_handler)
+                         collision_handler=collision_handler, movement_handler=movement_handler,
+                         on_game_end=save_score_to_file)
 
     def start(self):
         self.screen.show()
@@ -49,6 +70,6 @@ class AsteroidsGame:
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     asteroidsGame = AsteroidsGame(player_inputs=[PlayerInput(player_id="Steve", color="red"),
-                                                 PlayerInput(player_id="Urkel", color="yellow")])
+                                                 ])
     asteroidsGame.start()
     sys.exit(app.exec_())
