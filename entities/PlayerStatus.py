10425f4c865a02e1ce130217fb50addc7f30fbd0
Danilo Novakovic <dakenzi97@gmail.com>
2020-01-09 12:13:13 +0100

    Player Status is now colored in ship color

--- entities/PlayerStatus.py
+++ entities/PlayerStatus.py
@@ -2,14 +2,14 @@ from PyQt5 import QtWidgets
 
 
 class PlayerStatus:
-    def __init__(self, screen, x, y, padding=15):
+    def __init__(self, screen, x, y, padding=15, color="white"):
         self.x = x
         self.y = y
         self.padding = padding
         self.label = QtWidgets.QLabel(screen)
         self.label.setText("")
         self.label.setGeometry(x, y, 300, 20)
-        self.label.setStyleSheet(f"color: white;font-size: 12px; font-family: Arial Black; padding:{padding}px")
+        self.label.setStyleSheet(f"color: {color};font-size: 12px; font-family: Arial Black; padding:{padding}px")
 
     def update(self, name, num_lives, num_points):
         text = f"ID: {name} | L: {num_lives} | PTS: {num_points}"
